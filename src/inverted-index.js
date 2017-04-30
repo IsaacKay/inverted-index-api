6ec19a63d0a3a1d10a03031a65fc3fa956c74e2d
Bamidele <fisiwizy@gmail.com>
2017-05-01 00:13:02 +0100

    provide jsdoc for every  method

--- src/inverted-index.js
+++ src/inverted-index.js
@@ -1,14 +1,27 @@
-/* eslint require-jsdoc: 0*/
+/** InvertedIndex is a class representing a computer science concept
+ *  where the content of a file in mapped to it's position in data base
+ *  Or in this case, a JSON array.
+ */
 class InvertedIndex {
-  contructor() {
+  /**
+   * Initializes needed variables
+   */
+  constructor() {
     this.index = {};
-    this.token = '';
+    /** @private */
     this.books = {};
   }
-
+  /** @returns {object} books- the file content read */
   getBooks() {
     return this.books;
   }
+  /**
+   * @description It checks makes sure any file being uploaded is in good shape.
+   * This method is used by readFile(_);
+   * @returns {string|boolean} True when file if in good shape|Error string if not
+   * @param {object|string} books - representing file content or a stringified version
+   * of file content.
+    */
   isFileValid(books) {
     this.validity = true;
     if (!books || books === true) {
@@ -27,8 +40,7 @@ class InvertedIndex {
             const keysLength = Object.keys(book).length;
 
             /* book is malformed if it contains more than 1 key
-             * or if it does not cointain the title and text
-             */
+             * or if it does not cointain the title and text*/
             if (keysLength > 2 || !book.title || !book.text) {
               this.validity = 'Malformed File: The JSON file you passed in is out of shape. Please check again';
               break;
@@ -41,7 +53,13 @@ class InvertedIndex {
     }
     return this.validity;
   }
-
+  /**
+   * @description This method uses isFileValid() method to validate file
+   * before reading book
+   * @returns {object} object containing an error message if
+   * an error occurs while reading file and  file content
+   * @param {string|object} books reperesent file content
+   */
   readFile(books) {
     const validity = this.isFileValid(books);
     if (validity === true) {
@@ -52,6 +70,12 @@ class InvertedIndex {
     return this.books;
   }
 
+  /**
+   * @description This methods takes in file content. It should not be called until
+   * a file has been uploeaded successfully
+   * @returns {object} An object containing keys and corresponding indexes
+   * @param {object} books - uploaded file content
+   */
   createIndex(books) {
     const has = Object.prototype.hasOwnProperty;
     const readError = this.readFile(books).error;
@@ -81,7 +105,14 @@ class InvertedIndex {
     this.index = index;
     return index;
   }
-
+  /**
+   * @description This method first flattens nested arrays if provided.
+   * it break strings down into an array of words  then searches for each
+   * words in  the array. Then returns a map of each word and it occurrence.
+   * @param {string|array} searchTerms - An array or string containing
+   * search terms
+   * @returns {object} - An containing keys and position in the file
+   */
   searchIndex(...searchTerms) {
     const index = this.index;
     const searchResult = {};
@@ -100,7 +131,13 @@ class InvertedIndex {
     });
     return searchResult;
   }
-
+  /**
+   * @description  This method is used by searchIndex() to flatten the an array of search terms
+   * I.e if provided with [1,2,4,[5,6,7],8,9], it should return [1,2,3,4,5,6,7,8,9]
+   * @static
+   * @param {array} searchTerms - An array of search terms
+   * @returns {array} - flattend version of the paramenter
+   */
   static flattenSearchTerms(searchTerms = []) {
     const terms = searchTerms;
     let term = ''; // a term in searchTerms
@@ -108,8 +145,7 @@ class InvertedIndex {
       term = terms[i];
       /* the current term is an array remove it
        * from the array and replace it with it's content
-       * instead. then go over the array again.
-       */
+       * instead. then go over the array again.*/
       if (Array.isArray(term)) {
         terms.splice(i, 1, ...term);
         i = 0;
@@ -124,6 +160,13 @@ class InvertedIndex {
     return terms;
   }
 
+  /**
+   * @description This method checks if search can really take place or not
+   * For example, a search cannot be created when no inedex has been created.
+   * this method is used by searchIndex()
+   * @param {array} searchTerms an array of search terms
+   * @returns {string} an error if an error occurred or an empty string if not
+   */
   validateSearch(searchTerms = []) {
     let errorMessage = '';
     if (!searchTerms[0]) {
