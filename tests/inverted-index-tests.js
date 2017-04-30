dcbee3dd4f5b12728467b33c54147d9415c691be
Bamidele <fisiwizy@gmail.com>
2017-04-30 10:07:58 +0100

    correct wrong  test cases

--- tests/inverted-index-tests.js
+++ tests/inverted-index-tests.js
@@ -44,19 +44,19 @@ describe('InvertedIndex', () => {
   // this suit ensures that isFileValid works for as many input as possible
   describe('InvertedIndex.isFileValid', () => {
     it('Should return \'Invalid File: File must be a real JSON file\' when no argument is passed', () => {
-      expect(invertedIndex.isFileValid()).toEqual('Invalid File: File must be a real JSON file');
+      expect(invertedIndex.isFileValid()).toBe('Invalid File: File must be a real JSON file');
     });
 
     it('Should return \'Invalid File: File must be a real JSON file\' when null is passed as argument', () => {
-      expect(invertedIndex.isFileValid(null)).toEqual('Invalid File: File must be a real JSON file');
+      expect(invertedIndex.isFileValid(null)).toBe('Invalid File: File must be a real JSON file');
     });
 
     it('Should return \'Invalid File: File must be a real JSON file\' when boolean is passed as argument', () => {
-      expect(invertedIndex.isFileValid(true)).toEqual('Invalid File: File must be a real JSON file');
+      expect(invertedIndex.isFileValid(true)).toBe('Invalid File: File must be a real JSON file');
     });
 
     it('Should return \'Invalid File: File must be a real JSON file\' when boolean argument is passed', () => {
-      expect(invertedIndex.isFileValid(false)).toEqual('Invalid File: File must be a real JSON file');
+      expect(invertedIndex.isFileValid(false)).toBe('Invalid File: File must be a real JSON file');
     });
 
     it('Should return \'true\' when a valid argument is passed as', () => {
@@ -64,11 +64,11 @@ describe('InvertedIndex', () => {
     });
 
     it('Should return \'Empty File: The JSON File must not be empty\' an empty JSON file is passed', () => {
-      expect(invertedIndex.isFileValid(emptyJSONFile).toEqual('Empty File: The JSON File must not be empty'));
+      expect(invertedIndex.isFileValid(emptyJSONFile)).toBe('Empty File: The JSON File must not be empty');
     });
 
     it('Should return \'Invalid File: File must be a real JSON file\' when an Invalid json file is passed', () => {
-      expect(invertedIndex.isFileValid(invalidJSONFile)).toEqual('Invalid File: File must be a real JSON file');
+      expect(invertedIndex.isFileValid(invalidJSONFile)).toBe('Invalid File: File must be a real JSON file');
     });
 
     it('Should return \'Malformed File: The JSON file you passed in is out of shape. Please check again\' when a malformed File is passed', () => {
@@ -97,17 +97,35 @@ describe('InvertedIndex', () => {
 
 // this suit contains unit tests for createIndex method
 describe('InvertedIndex.createIndex', () => {
-  it('Should return an index when provided with valid filename and argument', () => {
+  it('Should return an index when provided with valid filename as argument', () => {
     const expectedIndex = {
       an: [0],
       inquiry: [0],
+      into: [0],
       the: [0, 1],
+      wealth: [0],
+      of: [0],
+      nations: [0],
+      this: [0, 1],
       string: [0, 1],
+      seeks: [0],
+      to: [0, 1],
+      help: [0, 1],
+      you: [0, 1],
+      understand: [0, 1],
+      problem: [0, 1],
+      set: [0, 1],
+      from: [1],
       third: [1],
       world: [1],
-      from: [1]
+      first: [1],
+      is: [1],
+      also: [1]
     };
+    expect(invertedIndex.createIndex(validJSONFile)).toEqual(expectedIndex);
+  });
 
-    expect(invertedIndex.createIndex('validJSONFile', validJSONFile)).toEqual(expectedIndex);
+  it('Should return an error message when cannot be created for any reason', () => {
+    expect(typeof invertedIndex.createIndex(malformedJSONFile)).toBe('string');
   });
 });
