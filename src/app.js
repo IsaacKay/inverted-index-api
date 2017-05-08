0bd2e9bfb3d1276fd441e69d6ce404d591f37cee
Bamidele <fisiwizy@gmail.com>
2017-05-08 22:34:14 +0100

    modularize route

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import express from 'express';
 import multer from 'multer';
 import bodyParser from 'body-parser';
 import InvertedIndex from './inverted-index';
-import JSONProcessor from './json-processor';
+import Controller from './controllers/controller';
 
 dotenv.config();
 
@@ -45,49 +45,11 @@ const invertedIndex = new InvertedIndex();
 let index;
 
 app.post('/api/create', (req, res) => {
-  try {
-    const isReqJSON = req.headers['content-type'].indexOf('application/json');
-    const isReqMultipart = req.headers['content-type'].indexOf('multipart/form-data');
-    if (isReqJSON > -1) {
-      const body = req.body;
-      // goto json-processor.js file to see how this class works
-      index = JSONProcessor.processRaw(body, invertedIndex);
-      return res.send(index);
-    } else if (isReqMultipart > -1) {
-      const files = req.files;
-      // goto json-processor.js file to see how this class works
-      index = JSONProcessor.processFiles(files, invertedIndex);
-      return res.send(index);
-    }
-  } catch (error) {
-    return res.send('invalid json file');
-  }
+  Controller.createIndex(req, res);
 });
 
 app.post('/api/search', (req, res) => {
-  try {
-    const body = req.body;
-    let searchResult = {};
-    if (!index) {
-      // if index is not yet created, send error message
-      return res.send('Please create an index. See documentation');
-    }
-    // the request body is an array
-    if (Array.isArray(body)) {
-      searchResult = invertedIndex.searchIndex(index, undefined, body);
-      return res.send(searchResult);
-    }
-    // if this line is reached, it means a javascript object is sent
-    const fileNames = Object.keys(body);
-    fileNames.forEach((fileName) => {
-      const searchTerms = body[fileName]; // searchTerm for one file
-      const result = invertedIndex.searchIndex(index, fileName, searchTerms);
-      searchResult[fileName] = result[fileName];
-    });
-    return res.send(searchResult);
-  } catch (error) {
-    return res.send('Invalid javascript object');
-  }
+  Controller.searchIndex(req, res);
 });
 
 // get port
